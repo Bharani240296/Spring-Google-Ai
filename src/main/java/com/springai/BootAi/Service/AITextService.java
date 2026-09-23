1c152140dd9a772559547b14784d1c7dfaa43d63
M. Bharanitharan <bharani240296@gmail.com>
2026-09-23 09:36:37 +0530

    initial commit:

--- src/main/java/com/springai/BootAi/Service/AITextService.java
+++ src/main/java/com/springai/BootAi/Service/AITextService.java
@@ -1,5 +1,7 @@
 package com.springai.BootAi.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.JsonReader;
 import org.springframework.ai.reader.TextReader;
@@ -12,7 +14,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AITextService {
@@ -20,28 +24,89 @@ public class AITextService {
     @Autowired
     private VectorStore vectorStore;
 
-    @Value("classpath:data/story.txt")
+    @Value("classpath:wrapData/story.txt")
     private Resource photoRes;
-
+private static  final Logger log= LoggerFactory.getLogger(AITextService.class);
     private boolean loaded = false;
 
     public List<Document> queryJson(String query) {
 
         if (!loaded) {
 
-            TextReader textReader=new TextReader(photoRes);
-
+            TextReader textReader = new TextReader(photoRes);
 
             List<Document> documents = textReader.get();
+
+            List<Document> finalDocuments = new ArrayList<>();
+            for (Document document : documents) {
+                log.info(documents.toString());
+
+                String content = document.getText();
+
+                String[] parts = content.split("(?i)Moral:");
+
+
+                String story = parts[0].trim();
+
+                if (!story.isEmpty()) {
+
+                    finalDocuments.add(
+                            new Document(
+                                    story,
+                                    Map.of("type", "story")
+                            )
+                    );
+                }
+
+                // Moral
+                if (parts.length > 1) {
+
+                    String moral = parts[1].trim();
+
+                    if (!moral.isEmpty()) {
+
+                        finalDocuments.add(
+                                new Document(
+                                        moral,
+                                        Map.of("type", "moral")
+                                )
+                        );
+                    }
+                }
+            }
+
             TokenTextSplitter splitter = TokenTextSplitter.builder()
                     .withChunkSize(500)
                     .withMinChunkSizeChars(350)
                     .withMinChunkLengthToEmbed(5)
                     .withMaxNumChunks(10000)
                     .build();
-vectorStore.add(splitter.apply(documents));
+
+            vectorStore.add(
+                    splitter.apply(finalDocuments)
+            );
+
             loaded = true;
         }
+
+        // =========================
+        // MORAL SEARCH
+        // =========================
+
+        if (query.toLowerCase().contains("moral")) {
+
+            return vectorStore.similaritySearch(
+                    SearchRequest.builder()
+                            .query("moral of the story")
+                            .topK(1)
+                            .filterExpression("type == 'moral'")
+                            .build()
+            );
+        }
+
+        // =========================
+        // NORMAL WORD SEARCH
+        // =========================
 
         return vectorStore.similaritySearch(
                 SearchRequest.builder()
